09ae843f3142f51d5f6ee8d5d6bed761d30087db
David Tran <gvdavid.tran@gmail.com>
2018-10-12 13:28:07 -0400

    added comments to code

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var io = socketIO(server);
 
 app.set('port', 5000);
 app.use(express.static(__dirname + '/public'));
+// Why the line below doesn't work the same as line above
 //app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
@@ -18,18 +19,22 @@ app.get('/', (req, res) => {
 
 var users = {};
 
+
 io.on('connection', (socket) => {
     console.log('a user has connected');
 
+    // Everytime new user connects, collect nickname entered and broadcast to rest of users
     socket.on('new user', (name) => {
         users[socket.id] = {nickname: name};
         socket.broadcast.emit('new user connected', users[socket.id].nickname);
     })
 
+    // Everytime a user disconnects
     socket.on('disconnect', () => {
         console.log('a user has disconnected');
     });
 
+    // Broadcast new messages to all users except sender
     socket.on('chat message', (msg) => {
         socket.broadcast.emit('chat message', {sender: users[socket.id].nickname, message: msg});
     });
--- public/js/Chat.js
+++ public/js/Chat.js
@@ -1,11 +1,16 @@
+// Only initialize socket after user has entered a nickname
+
+// On nickname submit, hide the nickname bar and initialize socket
 $('#nickname').submit(() => {
     $('#nickname').addClass('hidden');
     var nickname = $('#n').val();
     var socket = io();
     console.log('user online')
 
+    // Emit nickname to server
     socket.emit('new user', nickname);
 
+    // Emit message on Send
     $('#chat').submit(function(){
         var message = $('#m').val();
         $('#messages').append($('<li>').text(nickname + ': ' + message ));
@@ -14,16 +19,15 @@ $('#nickname').submit(() => {
         return false;
     });
 
+    // Everytime a message is received
     socket.on('chat message', (data) => {
         $('#messages').append($('<li>').text(data.sender + ': ' + data.message ));
     })
 
+    // Display name of new user, everytime new user connects to server
     socket.on('new user connected', (name) => {
         console.log(name)
         $('#messages').append($('<li>').text(name + ' has connected'));
     })
-
-
-
     return false;
 })
