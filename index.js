da95653a032ca322aa7b54f3387399cf50fadf17
David Tran <gvdavid.tran@gmail.com>
2018-11-01 09:51:24 -0400

    began displaying users in channels

--- index.js
+++ index.js
@@ -1,81 +1,107 @@
 // Dependencies
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var socketIO = require('socket.io');
+var express = require("express");
+var http = require("http");
+var path = require("path");
+var socketIO = require("socket.io");
 
 var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
 
-app.set('port', 5000);
-app.use(express.static(__dirname + '/public'));
+app.set("port", 5000);
+app.use(express.static(__dirname + "/public"));
 // Why the line below doesn't work the same as line above
 //app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-})
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
 
 var users = {};
+let channels = {
+    ch1: {
+        users: []
+    }
+};
 
 updateOnlineUsers = () => {
-    io.emit('online users', users);
-}
+    io.emit("online users", users);
+};
 
-io.on('connection', (socket) => {
-    console.log('a user has connected');
+updateChannels = () => {
+    io.emit("update channels", channels);
+};
+
+io.on("connection", socket => {
+    console.log("a user has connected");
 
     anotherUserIsNoLongerTyping = () => {
-        io.emit('anotherUserIsNoLongerTyping')
-    }
+        io.emit("anotherUserIsNoLongerTyping");
+    };
 
     // Everytime new user connects, collect nickname entered and broadcast to rest of users
-    socket.on('new user', (name) => {
-        users[socket.id] = {nickname: name, random: "boom"};
-        // console.log(users)
-        socket.broadcast.emit('new user connected', users[socket.id].nickname);
+    socket.on("new user", name => {
+        users[socket.id] = { nickname: name, channel: "ch1" };
+        socket.join(users[socket.id].channel);
+        channels.ch1.users.push(name);
+        console.log(channels);
+        socket
+            .to(users[socket.id].channel)
+            .emit("new user connected", users[socket.id].nickname);
         updateOnlineUsers();
-    })
+        updateChannels();
+    });
 
     // Everytime a user disconnects
-    socket.on('disconnect', () => {
-        console.log('a user has disconnected');
-        socket.broadcast.emit('user disconnected', users[socket.id].nickname);
-        anotherUserIsNoLongerTyping();
-        delete users[socket.id]
-        // console.log(users)
-        updateOnlineUsers();
+    socket.on("disconnect", () => {
+        if (users[socket.id]) {
+            console.log("a user has disconnected");
+            socket.broadcast.emit(
+                "user disconnected",
+                users[socket.id].nickname
+            );
+            anotherUserIsNoLongerTyping();
+            var index = channels.ch1.users.indexOf(users[socket.id].nickname);
+            if (index > -1) {
+                channels.ch1.users.splice(index, 1);
+            }
+            delete users[socket.id];
+            // console.log(users)
+            updateOnlineUsers();
+        }
     });
 
     // Broadcast new messages to all users except sender
-    socket.on('chat message', (msg) => {
-        socket.broadcast.emit('chat message', {sender: users[socket.id].nickname, message: msg});
+    socket.on("chat message", msg => {
+        socket.broadcast.emit("chat message", {
+            sender: users[socket.id].nickname,
+            message: msg
+        });
     });
 
-    // 
-    socket.on('userIsTyping', () => {
-        socket.broadcast.emit('anotherUserIsTyping', {sender: users[socket.id].nickname})
+    //
+    socket.on("userIsTyping", () => {
+        socket.broadcast.emit("anotherUserIsTyping", {
+            sender: users[socket.id].nickname
+        });
     });
 
-    
-    socket.on('userIsNoLongerTyping', () => {
+    socket.on("userIsNoLongerTyping", () => {
         anotherUserIsNoLongerTyping();
-    })
+    });
 
-    var socketInfo = 'socket info'
-    socket.on('joining ch1', (room) => {
-        console.log(`${socket.id} joining ${room}`)
+    var socketInfo = "socket info";
+    socket.on("joining ch1", room => {
+        console.log(`${socket.id} joining ${room}`);
         socket.join(room);
-        console.log(socket.room)
-        io.to(room).emit('room message', `${socket.id} joined ${room}`);
-    })
+        socket.leave(socket.id);
+        console.log(io.sockets.adapter.rooms);
+        io.to(room).emit("room message", `${socket.id} joined ${room}`);
+    });
 
     //io.to('ch1').emit('room message');
 });
 
-
-
 server.listen(5000, () => {
-    console.log('listening on :5000');
-})
+    console.log("listening on :5000");
+});
--- public/js/Chat.js
+++ public/js/Chat.js
@@ -1,41 +1,44 @@
 // Only initialize socket after user has entered a nickname
 
 // On nickname submit, hide the nickname bar and initialize socket
-$('#nickname').submit(() => {
-    $('#nickname').addClass('hidden');
-    var nickname = $('#n').val();
+$("#nickname").submit(() => {
+    $("#nickname").addClass("hidden");
+    var nickname = $("#n").val();
     var socket = io();
-    console.log('user online')
+    console.log("user online");
+    console.log(nickname);
 
     // Emit nickname to server
-    socket.emit('new user', nickname);
+    socket.emit("new user", nickname);
 
     // Emit message on Send
-    $('#chat').submit(function(){
-        var message = $('#m').val();
+    $("#chat").submit(function() {
+        var message = $("#m").val();
         if (message) {
-            $('#messages').append($('<li>').text(nickname + ': ' + message ));
-            socket.emit('chat message', message);
-            $('#m').val('');
+            $("#messages").append($("<li>").text(nickname + ": " + message));
+            socket.emit("chat message", message);
+            $("#m").val("");
             timeoutFunction();
         }
         return false;
     });
 
     // Everytime a message is received
-    socket.on('chat message', (data) => {
-        $('#messages').append($('<li>').text(data.sender + ': ' + data.message ));
-    })
+    socket.on("chat message", data => {
+        $("#messages").append(
+            $("<li>").text(data.sender + ": " + data.message)
+        );
+    });
 
     // Display name of new user, everytime new user connects to server
-    socket.on('new user connected', (name) => {
+    socket.on("new user connected", name => {
         // console.log(name)
-        $('#messages').append($('<li>').text(name + ' has connected'));
-    })
+        $("#messages").append($("<li>").text(name + " has connected"));
+    });
 
-    socket.on('user disconnected', (name) => {
-        $('#messages').append($('<li>').text(name + ' has disconnected'));
-    })
+    socket.on("user disconnected", name => {
+        $("#messages").append($("<li>").text(name + " has disconnected"));
+    });
 
     // onKeyDown
     var typing = false;
@@ -44,57 +47,68 @@ $('#nickname').submit(() => {
     timeoutFunction = () => {
         if (typing == true) {
             typing = false;
-            socket.emit('userIsNoLongerTyping');
+            socket.emit("userIsNoLongerTyping");
         }
-    }
+    };
 
-    $('#m').keydown((e) => {
+    $("#m").keydown(e => {
         // only record typing if enter not pressed
-        if(e.which!=13 && e.keyCode!=13) {
+        if (e.which != 13 && e.keyCode != 13) {
             if (typing == false) {
-                typing = true
-                socket.emit('userIsTyping');
+                typing = true;
+                socket.emit("userIsTyping");
                 timeout = setTimeout(timeoutFunction, 5000);
             } else {
                 clearTimeout(timeout);
                 timeout = setTimeout(timeoutFunction, 5000);
             }
-        }   
+        }
     });
 
     // another user is typing
-    socket.on('anotherUserIsTyping', (data) => {
-        $('.is-typing').html(data.sender + ' is typing...');
-    })
+    socket.on("anotherUserIsTyping", data => {
+        $(".is-typing").html(data.sender + " is typing...");
+    });
 
     // another user is no longer typing
-    socket.on('anotherUserIsNoLongerTyping', () => {
-        $('.is-typing').html('');
-    })
+    socket.on("anotherUserIsNoLongerTyping", () => {
+        $(".is-typing").html("");
+    });
 
     // update online users list
-    socket.on('online users', (users) => {
+    socket.on("online users", users => {
         var userList = "";
         // console.log(`userList = ${userList}`)
         for (var user in users) {
-            var nickname = users[user].nickname
+            var nickname = users[user].nickname;
             // console.log(`nickname = ${nickname}`)
             userList += `<li>${nickname}</li>`;
             // console.log(`userList = ${userList}`)
         }
-        $('#online').html(userList);
-    })
-    
-    $('#ch1').click(() => {
-        var room = 'ch1';
-        console.log('clicked!!');
-        socket.emit('joining ch1', room);
+        $("#online").html(userList);
+    });
 
-    })
+    $("#ch1").click(() => {
+        var room = "ch1";
+        console.log("clicked!!");
+        socket.emit("joining ch1", room);
+    });
 
-    socket.on('room message', (data) => {
-        $('#messages').append($('<li>').text(data));
-    }) 
+    socket.on("room message", data => {
+        $("#messages").append($("<li>").text(data));
+    });
+
+    socket.on("update channels", channels => {
+        for (channel in channels) {
+            $("#channel-list")
+                .append("<li>" + channel + "</li>")
+                .appendChild(subul);
+            var subul = document.createElement("ul");
+            for (user in channel) {
+                subul.append("<li>item</li>");
+            }
+        }
+    });
 
     return false;
-})
+});
